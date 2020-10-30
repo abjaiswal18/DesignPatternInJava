b648c5ecc420a6cef34cdc8e82cce5f4518bdaed
Abhishek <abjaiswal1997@gmail.com>
2020-10-31 00:49:32 +0530

    FlyWeight Design Pattern

---
+++ src/FlyweightPattern/Circle.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FlyweightPattern;
+
+/**
+ *
+ * @author HP
+ */
+public class Circle implements Shape{
+    
+    private String color;
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public void draw() {
+        System.out.println(x+" "+y+" "+radius+" "+color);
+    }
+    
+    
+}
