edcf3d2f534b40fe93a81f87d15c2f18fd78ee6d
Abhishek <abjaiswal1997@gmail.com>
2020-11-01 01:23:28 +0530

    State Design Pattern

---
+++ src/StatePattern/StartState.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StatePattern;
+
+/**
+ *
+ * @author HP
+ */
+public class StartState implements State{
+    
+    public void doAction(Context context){
+        System.out.println("Player is in start state");
+        context.setState(this);
+    }
+
+    @Override
+    public String toString() {
+        return "StartState{" + '}';
+    }
+    
+    
+}
---
+++ src/StatePattern/StopState.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StatePattern;
+
+/**
+ *
+ * @author HP
+ */
+public class StopState implements State{
+    
+    public void doAction(Context context){
+        System.out.println("Player is in stop state");
+        context.setState(this);
+    }
+
+    @Override
+    public String toString() {
+        return "StopState{" + '}';
+    }
+    
+    
+}
